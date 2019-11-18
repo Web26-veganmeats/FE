29f7a9b4393f5f45ea962517a77ef18737051c60
Robbie Porter <rthomporter@gmail.com>
2019-11-17 23:07:41 -0600

    Login/Register built/linked to backend

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -1,12 +1,70 @@
-import React from 'react';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import axios from "axios";
 
+const validate = ({ username, password }) => {
+  const errors = {};
+
+  if (!username) {
+    errors.username = "Username is required";
+  }
+  if (!password) {
+    errors.password = "Password is required";
+  } else if (password.length < 5) {
+    errors.password = "Password must have at least 5 characters";
+  }
+  return errors;
+};
 
 const Login = () => {
-    return (
-        <div>
+  const handleSubmit = (values, tools) => {
+    axios
+      .post("https://veganmeets-buildweek.herokuapp.com/api/auth/login", values)
+      .then(response => {
+        console.log(response);
+        tools.resetForm();
+      })
+      .catch(error => {
+        alert(error.message);
+      })
+      .finally(() => {
+        tools.setSubmitting(false);
+      });
+  };
 
-        </div>
-    )
-}
+  return (
+    <Formik
+      onSubmit={handleSubmit}
+      validate={validate}
+      initialValues={{ username: "", password: "" }}
+      render={props => {
+        return (
+          <div>
+            <Form className="signup-form">
+              <label htmlFor="username">Sign In</label>
+              <ErrorMessage name="username" component="div" className="error" />
+              <Field
+                className="field"
+                type="text"
+                name="username"
+                placeholder="Username"
+              />
+              <ErrorMessage name="password" component="div" className="error" />
+              <Field
+                className="field"
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              <button type="submit" disabled={props.isSubmitting}>
+                {props.isSubmitting ? "Submitting" : "Submit"}
+              </button>
+            </Form>
+          </div>
+        );
+      }}
+    />
+  );
+};
 
 export default Login;
--- app/src/components/Register.jsx
+++ app/src/components/Register.jsx
@@ -1,12 +1,85 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import axios from "axios";
 
+const SignupDiv = styled.div`
+  margin-top: 20vh;
+`;
+
+const P = styled.p`
+  font-style: italic;
+`;
+
+const validate = ({ username, password }) => {
+  const errors = {};
+
+  if (!username) {
+    errors.username = "Username is required";
+  }
+  if (!password) {
+    errors.password = "Password is required";
+  } else if (password.length < 5) {
+    errors.password = "Password must have at least 5 characters";
+  }
+  return errors;
+};
 
 const Register = () => {
-    return (
-        <div>
+  const handleSubmit = (values, tools) => {
+    axios
+      .post(
+        "https://veganmeets-buildweek.herokuapp.com/api/auth/register",
+        values
+      )
+      .then(response => {
+        console.log(response);
+        tools.resetForm();
+      })
+      .catch(error => {
+        alert(error.message);
+      })
+      .finally(() => {
+        tools.setSubmitting(false);
+      });
+  };
 
-        </div>
-    )
-}
+  return (
+    <Formik
+      onSubmit={handleSubmit}
+      validate={validate}
+      initialValues={{ username: "", password: "" }}
+      render={props => {
+        return (
+          <SignupDiv>
+            <h1>Vegan Meets</h1>
+            <h2>Sign up to find vegan food in your area</h2>
+            <Form className="signup-form">
+              <label htmlFor="username">Sign Up</label>
+              <ErrorMessage name="username" component="div" className="error" />
+              <Field
+                className="field"
+                type="text"
+                name="username"
+                placeholder="Username"
+              />
+              <ErrorMessage name="password" component="div" className="error" />
+              <Field
+                className="field"
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              <button type="submit" disabled={props.isSubmitting}>
+                {props.isSubmitting ? "Submitting" : "Submit"}
+              </button>
+            </Form>
+            <P>or</P>
+          </SignupDiv>
+        );
+      }}
+    />
+  );
+};
 
 export default Register;
