eff24ce8c6bab3f271179bd05915dc9ac5200575
marielapg <mpgonzales77@gmail.com>
2019-11-18 15:30:56 -0800

    resturant card

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ResturantCard from "./components/ResturantCard";
 
 function App() {
   return (
     <div className="App">
-      
+      <ResturantCard/>
+    
     </div>
   );
 }
