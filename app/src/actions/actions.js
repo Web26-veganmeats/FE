0ca034df5c67b3a9ced4d8829fa60d819fc3748b
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-11-20 16:00:32 -0600

    added async action creator

--- app/src/actions/actions.js
+++ app/src/actions/actions.js
@@ -1,4 +1,5 @@
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 // Action Types 
 export const FETCH_REST_LOADING = 'FETCH_REST_LOADING';
@@ -8,6 +9,10 @@ export const FETCH_REST_FAILURE = 'FETCH_REST_FAILURE';
 export const CREATE_REST_START = 'CREATE_REST_START'
 export const CREATE_REST_SUCCESS = 'CREATE_REST_SUCCESS'
 export const CREATE_REST_FAILURE = 'CREATE_REST_FAILURE'
+
+export const DELETE_REST_START = 'DELETE_REST_START'
+export const DELETE_REST_SUCCESS = 'DELETE_REST_SUCCESS'
+export const DELETE_REST_FAILURE = 'DELETE_REST_FAILURE'
 
 // Async action creators
 
@@ -30,3 +35,13 @@ export const createRest = (newRest) => dispatch => {
         .catch(error => dispatch({ type: CREATE_REST_FAILURE, payload: error }))
 }
 
+export const deleteRest = id => dispatch => {
+    dispatch({ type: DELETE_REST_START })
+    axiosWithAuth()
+        .delete(`https://veganmeets-buildweek.herokuapp.com/api/restaurants/delete/${id}`)
+        .then(response => {
+            console.log('Delete Restaurant Response: ', response)
+            dispatch({ type: DELETE_REST_SUCCESS, payload: response.data })
+        })
+        .catch(error => dispatch({ type: DELETE_REST_FAILURE, payload: error }))
+}
