fcb540981931d6dd2a8049715859180b5e06b5fc
Robbie Porter <rthomporter@gmail.com>
2019-11-21 12:14:04 -0600

    Login page linked to register + small styling fix on restaurant list page

--- app/src/components/RestaurantList.jsx
+++ app/src/components/RestaurantList.jsx
@@ -39,9 +39,8 @@ const ListDivs = styled.div`
   margin: 2% 10% 2% 10%;
 `;
 
-const ResturantList = (props) => {
+const ResturantList = props => {
   // console.log('Resturant List Props:', props)
-
 
   const [restaurants, setRestaurants] = useState([]);
 
@@ -106,10 +105,13 @@ const ResturantList = (props) => {
       </section>
     );
   }
-  return ( 
+  return (
     <div>
       <img src={background} alt="background" className="list_background" />
       <NavBar />
+      <Link to="/addrestform">
+        <Button>Add Restaurant</Button>
+      </Link>
       <Form>
         <Input
           id="search"
@@ -120,8 +122,6 @@ const ResturantList = (props) => {
           value={searchTerm}
         />
       </Form>
-
-      <Link to='/addrestform'><Button>Add Restaurant</Button></Link>
 
       <section>{listRender}</section>
     </div>
