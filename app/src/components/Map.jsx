75bfa4ebde8d95eab3bcbeb7e30e189efcd3754b
Robbie Porter <rthomporter@gmail.com>
2019-11-21 17:48:23 -0600

    Map updates on the wrong branch - ignore

--- app/src/components/Map.jsx
+++ app/src/components/Map.jsx
@@ -1,43 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
-import Geocode from "react-geocode";
-
-Geocode.setLanguage("en");
-Geocode.setRegion("us");
-Geocode.setApiKey("AIzaSyBgIzCkaMx7LAt5OG8l3q7UuJZLW7yhysM");
+import axios from "axios";
 
 var mapStyles = {
   width: "55%",
   height: "400px",
-  marginTop: "70px"
+  marginTop: "5%"
 };
 
 const Maps = props => {
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
-
-  Geocode.fromAddress(props.rest.street_address).then(
-    response => {
-      const { lat, lng } = response.results[0].geometry.location;
-      console.log(lat, lng);
-      setLat(lat);
-      setLng(lng);
-    },
-    error => {
-      console.error(error);
-    }
-  );
-
   console.log(lat, lng);
+
+  const addressStr = `${props.rest.street_address
+    .split(" ")
+    .join("+")},+${props.rest.city.split(" ").join("+")},+${props.rest.state}`;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${addressStr}&key=AIzaSyBgIzCkaMx7LAt5OG8l3q7UuJZLW7yhysM`
+      )
+      .then(response => {
+        console.log("response", response.data.results[0].geometry.location);
+        setLat(response.data.results["0"].geometry.location.lat);
+        setLng(response.data.results["0"].geometry.location.lng);
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  }, []);
+
+  const latNum = parseFloat(lat);
+  const lngNum = parseFloat(lng);
 
   return (
     <Map
       google={props.google}
-      zoom={8}
+      zoom={12}
       style={mapStyles}
-      initialCenter={{ lat: { lat }, lng: { lng } }}
+      initialCenter={{ lat: { latNum }, lng: { lngNum } }}
     >
-      <Marker position={{ lat: { lat }, lng: { lng } }} />
+      <Marker position={{ lat: { latNum }, lng: { lngNum } }} />
     </Map>
   );
 };
